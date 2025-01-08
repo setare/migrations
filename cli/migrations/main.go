092c5137a4eb669f2b991021bcfa3f5c90839924
Jamillo Santos <jota@setare.io>
2025-01-08 19:21:18 -0300

    fix: Move the cmd to cli

---
+++ cli/migrations/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jamillosantos/migrations/v2/cli/internal/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
--- cmd/migrations/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/jamillosantos/migrations/v2/internal/cmd"
-
-func main() {
-	cmd.Execute()
-}
